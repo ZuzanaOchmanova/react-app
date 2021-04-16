48ccdd5602bde1d6685225d3d7a845187528e4a9
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-16 17:06:49 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App"> Weather App
+    <footer>
+      This project was coded by {""}
+      <a href="https://www.linkedin.com/in/zuzanaochmanova/" 
+      target="_blank">
+      Zuzana Ochmanova </a>
+      and is {""}
+    <a href="https://github.com/ZuzanaOchmanova/react-app"
+    target="_blank">
+      Open-sourced on GitHub</a>
+</footer>
     </div>
   );
 }
