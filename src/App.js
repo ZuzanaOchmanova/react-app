e8de6f29751465b03fa7bec290551b896bf2d236
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-16 18:20:06 +0200

    Basic outline of the weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Weather from "./Weather"
+import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
+
 
 export default function App() {
   return (
-    <div className="App"> Weather App
-    <Weather />
-    <footer>
-      This project was coded by {""}
-      <a href="https://www.linkedin.com/in/zuzanaochmanova/" 
-      target="_blank"
+  <div className="App"> 
+    <div className="container">
+     <Weather />
+<footer>
+       This project was coded by {""}
+       <a href="https://www.linkedin.com/in/zuzanaochmanova/" 
+       target="_blank"
          rel="noopener noreferrer">
-      Zuzana Ochmanova </a> 
-      and is {""}
-    <a href="https://github.com/ZuzanaOchmanova/react-app"
-    target="_blank"
+       Zuzana Ochmanova </a> 
+        and is {""}
+     <a href="https://github.com/ZuzanaOchmanova/react-app"
+     target="_blank"
        rel="noopener noreferrer">
       Open-sourced on GitHub</a>
 </footer>
     </div>
+  </div>
   );
 }
 
--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,43 @@ import "./Weather.css";
 
 export default function Weather() {
     return (
-        <div className="Weather">Hello from Weather</div>
+        <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                <input 
+                type="search" 
+                placeholder="Enter a city"
+                className="form-control"/>
+                 </div>
+                 <div className="col-3">
+                <input type="submit" 
+                value="Search" 
+                className="btn btn-primary"></input>
+                </div>
+                 </div>   
+            </form>
+            <h1>Aarhus</h1>
+            <ul>
+                <li>Wednesday 08:00</li>
+                <li> Sunny</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                    alt="Sunny"
+                    />
+                    6°C
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li> Precipitation: 15% </li>
+                        <li> Humidity: 72% </li>
+                        <li> Wind: 13 km/h </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
     );
 }
