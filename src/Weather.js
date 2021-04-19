6898f509f56bcd52baa5498d7e75da13aef2aac5
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-19 17:25:53 +0200

    Changing weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
             wind: response.data.wind.speed,
             city: response.data.name,
             describtion: response.data.weather[0].description,
-            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         });
     }
 
