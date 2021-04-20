d279aac0b80eaac2ce3e04224cf8afb9ac37e07c
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-20 17:03:17 +0200

    Adding missed return

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -30,6 +30,8 @@ export default function WeatherForecast(props) {
               <WeatherForecastDay data={dailyForecast} />
             </div>
             );
+            } else {
+                return null;
             }
             })}         
         </div>
