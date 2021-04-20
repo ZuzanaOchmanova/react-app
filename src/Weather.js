c30a4ef683c1789e92f02c6fccc20fbe3ba113e4
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-20 15:48:42 +0200

    Layout for forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./Weatherinfo";
+import WeatherForecast from "./WeatherForecast";
+
 
 
 export default function Weather(props) {
@@ -18,7 +20,7 @@ export default function Weather(props) {
             wind: response.data.wind.speed,
             city: response.data.name,
             describtion: response.data.weather[0].description,
-            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            icon: response.data.weather[0].icon,
         });
     }
 
@@ -59,6 +61,9 @@ export default function Weather(props) {
                  </div>   
             </form>    
             <WeatherInfo data={weatherData}/>
+            
+             <WeatherForecast />
+          
             </div>
     );
     } else {
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,28 +1,28 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon.js";
 
 export default function WeatherInfo(props) {
     return (
         <div className="WeatherInfo">
              <h1>{props.data.city}</h1>
             <ul>
-                <li><FormattedDate date={props.data.date} />
+                <li>
+                <FormattedDate date={props.data.date} />
                 </li>
                 <li className="text-capitalize">{props.data.describtion}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="clearfix">
-                    <img
-                    src={props.data.iconUrl}
-                    alt="{props.data.description}"
-                  className="float-left"
-                    />
-                <div className="float-left">
-                    <WeatherTemperature celsius={props.data.temperature}/>
-                 </div>
-                  </div>
+                         <div className="float-left">
+                             <WeatherIcon code={props.data.icon} size={52} />
+                         </div>
+                            <div className="float-left">
+                            <WeatherTemperature celsius={props.data.temperature}/>
+                        </div>
+                     </div>
                 </div>
                 <div className="col-6">
                     <ul>
