7289267ac0fb93d973e3bf48a2d3c52b68db1f1b
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-22 14:20:21 +0200

    Changing CSS to Jarvis

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
+      color="#a5e7ff"
       size={props.size}
       animate={true}
     />
